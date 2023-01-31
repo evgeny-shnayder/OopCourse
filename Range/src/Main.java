ea53b0070f4ce06b8ec73c73d390b8a2ac7ce3fb
colemanl <evgshnajder@gmail.com>
2023-01-31 22:08:02 +0700

    Задача 2-я часть

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -7,17 +7,41 @@ public class Main {
         double number = 4.98;
         String answer = range.isInside(number) ? "Да" : "Нет";
 
-        System.out.println("Длина промежутка: " + range.rangeLength(range.getFrom(), range.getTo()));
+        System.out.println("Длина промежутка: " + range.rangeLength());
         System.out.println("Число " + number + " принадлежит промежутку от " + range.getFrom() + " до " + range.getTo()
                 + ": " + answer);
 
-        range.setTo(4.9);
-        range.setFrom(4.2);
+        range.setFrom(3.0);
+        range.setTo(5.21);
 
         answer = range.isInside(number) ? "Да" : "Нет";
 
-        System.out.println("Длина промежутка: " + range.rangeLength(range.getFrom(), range.getTo()));
+        System.out.println("Длина промежутка: " + range.rangeLength());
         System.out.println("Число " + number + " принадлежит промежутку от " + range.getFrom() + " до " + range.getTo()
                 + ": " + answer);
+
+        Range range2 = new Range(3.0, 5.21);
+
+        if (range.getCrossingRange(range2) != null) {
+            Range mergeRange = range.getCrossingRange(range2);
+            System.out.println("Интервал - пересечение: " + mergeRange.getFrom() + " " + mergeRange.getTo());
+        } else {
+            System.out.println("Пересечения числовых интервалов от " + range.getFrom() + " до " + range.getTo() + " и "
+                    + range2.getFrom() + " до " + range2.getTo() + " нет");
+        }
+
+        for (Range e : range.getUnionRange(range2)) {
+            System.out.println("Объединение числовых интервалов: " + e.getFrom() + " " + e.getTo());
+        }
+
+        if (range.getRangeDifference(range2) != null) {
+            System.out.print("Разность числовых интервалов:");
+
+            for (Range e : range.getRangeDifference(range2)) {
+                System.out.print(" от " + e.getFrom() + " до " + e.getTo());
+            }
+        } else {
+            System.out.println("Разность числовых интервалов равна нулю.");
+        }
     }
 }
