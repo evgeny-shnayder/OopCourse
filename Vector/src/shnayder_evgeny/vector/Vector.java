c1cdb6dfcb36efa9fb76421b1fdda7db77a21eae
colemanl <evgshnajder@gmail.com>
2023-02-28 14:06:55 +0700

    Вектор

--- Vector/src/shnayder_evgeny/vector/Vector.java
+++ Vector/src/shnayder_evgeny/vector/Vector.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Vector {
     private double[] vector;
 
-    final int size;
+    private int size;
 
     public Vector(int size) {
         if (size <= 0) {
@@ -17,16 +17,28 @@ public class Vector {
     }
 
     public Vector(Vector vector) {
+        if (vector.size <= 0) {
+            throw new IllegalArgumentException("Длина массива не может быть меньше или равна нулю.");
+        }
+
         this.size = vector.vector.length;
         this.vector = vector.vector;
     }
 
     public Vector(double[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Длина массива не может быть меньше или равна нулю.");
+        }
+
         this.size = array.length;
         this.vector = array;
     }
 
     public Vector(int size, double[] array) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Длина массива не может быть меньше или равна нулю.");
+        }
+
         this.size = size;
         vector = Arrays.copyOf(array, size);
     }
@@ -37,50 +49,40 @@ public class Vector {
 
     public void setVector(double[] vector) {
         this.vector = vector;
+        this.size = vector.length;
     }
 
     public int getSize() {
         return size;
     }
 
-    private double[] getSumOrDifference(double[] array1, double [] array2, int sign) {
-        double[] resultArray = array1;
+    private static double[] getSumOrDifference(double[] array1, double[] array2, int sign1, int sign2) {
+        double[] needArray = Arrays.copyOf(array1, array2.length);
 
-        for (int i = 0; i < array1.length; i++) {
-            resultArray[i] += array2[i] * sign;
+        for (int i = 0; i < needArray.length; i++) {
+            needArray[i] = needArray[i] * sign1 + array2[i] * sign2;
         }
 
-        return resultArray;
+        return needArray;
     }
+
     public Vector getSum(Vector vector) {
-        double[] needVector;
-
         if (size < vector.size) {
-            needVector = Arrays.copyOf(this.vector, vector.size);
-
-            return new Vector(getSumOrDifference(needVector, vector.vector, 1));
+            return new Vector(getSumOrDifference(this.vector, vector.vector, 1, 1));
         }
 
-        needVector = Arrays.copyOf(vector.vector, size);
-
-        return new Vector(getSumOrDifference(needVector, this.vector, 1));
+        return new Vector(getSumOrDifference(vector.vector, this.vector, 1, 1));
     }
 
     public Vector getDifference(Vector vector) {
-        double[] needVector;
-
         if (size < vector.size) {
-            needVector = Arrays.copyOf(this.vector, vector.size);
-
-            return new Vector(getSumOrDifference(needVector, vector.vector, -1));
+            return new Vector(getSumOrDifference(this.vector, vector.vector, 1, -1));
         }
 
-        needVector = Arrays.copyOf(vector.vector, size);
-
-        return new Vector(getSumOrDifference(needVector, this.vector, -1));
+        return new Vector(getSumOrDifference(vector.vector, this.vector, -1, 1));
     }
 
-    public Vector getMultiplication (int scalar) {
+    public Vector getMultiplication(int scalar) {
         double[] needVector = Arrays.copyOf(this.vector, size);
 
         for (int i = 0; i < size; i++) {
@@ -96,11 +98,86 @@ public class Vector {
         for (double e : vector) {
             squaresSum += e;
         }
+
         return Math.sqrt(squaresSum);
+    }
+
+    public double getIndexValue(int index) {
+        return vector[index];
+    }
+
+    public void setIndexValue(int index, double value) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Заданный индекс больше размерности вектора.");
+        }
+
+        Arrays.fill(vector, index, index + 1, value);
     }
 
     @Override
     public String toString() {
         return Arrays.toString(vector).replace("[", "{").replace("]", "}");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object == null || object.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) object;
+
+        for (int i = 0; i < vector.size; i++) {
+            if (vector.vector[i] != this.vector[i]) {
+                return false;
+            }
+        }
+
+        return vector.size == size;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+
+        for (double e : vector) {
+            hash *= prime + Double.hashCode(e);
+        }
+
+        return hash * prime + Double.hashCode(size);
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        if (vector1.size < vector2.size) {
+            return new Vector(getSumOrDifference(vector1.vector, vector2.vector, 1, 1));
+        }
+
+        return new Vector(getSumOrDifference(vector2.vector, vector1.vector, 1, 1));
+    }
+
+    public static Vector getDifference(Vector vector1, Vector vector2) {
+        if (vector1.size < vector2.size) {
+            return new Vector(getSumOrDifference(vector1.vector, vector2.vector, 1, -1));
+        }
+
+        return new Vector(getSumOrDifference(vector2.vector, vector1.vector, -1, 1));
+    }
+
+    public static Vector getMultiplication(Vector vector1, Vector vector2) {
+        int minLength = Math.min(vector1.size, vector2.size);
+        double[] needVector = new double[minLength];
+
+        for (int i = 0; i < minLength; i++) {
+            needVector[i] = vector1.vector[i] * vector2.vector[i];
+        }
+
+        needVector = Arrays.copyOf(needVector, Math.max(vector1.size, vector2.size));
+
+        return new Vector(needVector);
     }
 }
--- Vector/src/shnayder_evgeny/vector_main/Main.java
+++ Vector/src/shnayder_evgeny/vector_main/Main.java
@@ -4,34 +4,44 @@ import shnayder_evgeny.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(4);
+        Vector vector1 = new Vector(4);
 
-        vector.setVector(new double[]{1});
+        vector1.setVector(new double[]{1});
+
+        System.out.println("Вектор: " + vector1 + " имеет размерность: " + vector1.getSize());
 
         double[] array = new double[]{1.0, 2.0, 3.1, 5.2, 7};
 
-        Vector vector2 = new Vector(array);
+        Vector vector3 = new Vector(array);
 
-        Vector vector1 = new Vector(6, array);
+        Vector vector2 = new Vector(6, vector3.getVector());
 
-        System.out.println(vector);
+        System.out.println("Вектор номер 2: " + vector3);
 
-        System.out.println(vector2);
+        vector2.setIndexValue(5, 5.2);
 
-        System.out.println(vector1);
+        System.out.println("Вектор номер 1: " + vector2);
 
-        System.out.println("Сумма двух векторов: " + vector2.getSum(vector1));
+        System.out.println("Сумма двух векторов: " + vector3.getSum(vector2));
 
-        System.out.println(vector2);
+        System.out.println("Разность двух векторов: " + vector3.getDifference(vector2));
 
-        System.out.println(vector1);
+        System.out.println("Умножение вектора на число: " + vector2.getMultiplication(2));
 
-        System.out.println("Разность двух векторов: " + vector2.getDifference(vector1));
+        System.out.println("Длина вектора: " + vector2.getLength());
 
-        System.out.println("Умножение вектора на число: " + vector1.getMultiplication(2));
+        System.out.println("Разворот вектора: " + vector2.getMultiplication(-1));
 
-        System.out.println("Длина вектора: " + vector1.getLength());
+        Vector vector4 = new Vector(vector2);
 
-        System.out.println("Разворот вектора: " + vector1.getMultiplication(-1));
+        vector4.setIndexValue(4, 9.2);
+
+        System.out.println(vector4.getIndexValue(4));
+
+        System.out.println("Сложение двух векторов: " + Vector.getSum(vector4, vector3));
+
+        System.out.println("Вычитание двух векторов: " + Vector.getDifference(vector4, vector3));
+
+        System.out.println("Умножение двух векторов: " + Vector.getMultiplication(vector2, vector1));
     }
 }
