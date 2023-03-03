6bcfe3900959218741dfd7e56ab43c0bdaf9247e
colemanl <evgshnajder@gmail.com>
2023-03-03 16:53:36 +0700

    1. Изменил название пакета на shapes_comparator. 2. Изменил имена AreaComparator и PerimeterComparator. - переделал на Double.compare(); 3. Изменил название аргументов на shape1 и shape2 в AreaComparator. 4. В Square добавил getSideLength и setSideLength 5. Переделал метод getArea по другой формуле. Так же изменил метод getPerimeter 6. Triangle.toString исправил "(" + x1 + "; " + y1 + ")," 7. Сгруппировал сравнение по осям для лучшей читаемости в классе Triangle.equals. 8. Добавил аннотацию @OverRide Triangle.hashCode 9. Main.getSecondLargestPerimeterShape добавил проверку для массива состоящего из одного элемента. 10. Удалил переменные maxArea и MaxPerimeter 11. Исправил на: "Фигура с максимальной площадью:" 12. Triangle.getSideLength - удалил. т.к. переделал метод getArea 13. Ошибку исправил

--- Shape/src/evgeny_shnayder/shapes/Square.java
+++ Shape/src/evgeny_shnayder/shapes/Square.java
@@ -3,15 +3,15 @@ package evgeny_shnayder.shapes;
 public class Square implements Shape {
     private double sideLength;
 
+    public Square(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
     public double getSideLength() {
         return sideLength;
     }
 
     public void setSideLength(double sideLength) {
-        this.sideLength = sideLength;
-    }
-
-    public Square(double sideLength) {
         this.sideLength = sideLength;
     }
 
