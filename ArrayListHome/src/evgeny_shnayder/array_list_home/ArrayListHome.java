3f56a1085fe40cea8654bd6a2f41624a84758436
colemanl <evgshnajder@gmail.com>
2023-04-02 16:52:51 +0700

    1. array_list - переименовал array_list_home 2. recordFromFile: - переименовал в getFileLines - при пустом файле создается пустой список 3. цепочку if удалил 4. getFileLines использовал BufferedReader вместо Scanner 5. getListWithoutRepeats переделал через stream

--- ArrayListHome/src/evgeny_shnayder/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/evgeny_shnayder/array_list_home/ArrayListHome.java
@@ -1,10 +1,10 @@
-package evgeny_shnayder.array_list;
+package evgeny_shnayder.array_list_home;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ArrayListHome {
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class ArrayListHome {
         String fileName = scanner.nextLine();
 
         try {
-            System.out.println(recordFromFile(fileName));
+            System.out.println(getFileLines(fileName));
         } catch (FileNotFoundException e) {
             System.out.println("Файл с именем " + fileName + " не найден. Введите правильное имя файла!");
         }
@@ -29,19 +29,10 @@ public class ArrayListHome {
         System.out.println(getListWithoutRepeats(list));
     }
 
-    public static ArrayList<String> recordFromFile(String fileName) throws FileNotFoundException {
-        Scanner file = new Scanner(new FileInputStream(fileName));
-        if (!file.hasNext()) {
-            throw new RuntimeException("Файл " + fileName + " пустой.");
-        } else {
-            ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<String> getFileLines(String fileName) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
 
-            while (file.hasNext()) {
-                list.add(file.next());
-            }
-
-            return list;
-        }
+        return bufferedReader.lines().collect(Collectors.toCollection(ArrayList::new));
     }
 
     public static void removeEvenNumbers(ArrayList<Integer> list) {
@@ -54,14 +45,6 @@ public class ArrayListHome {
     }
 
     public static ArrayList<Integer> getListWithoutRepeats(ArrayList<Integer> list) {
-        ArrayList<Integer> listWithoutRepeats = new ArrayList<>(list);
-
-        for (int i = 0; i < listWithoutRepeats.size(); i++) {
-            while (listWithoutRepeats.lastIndexOf(listWithoutRepeats.get(i)) != i) {
-                listWithoutRepeats.remove(i);
-            }
-        }
-
-        return listWithoutRepeats;
+        return list.stream().distinct().collect(Collectors.toCollection(ArrayList::new));
     }
 }
