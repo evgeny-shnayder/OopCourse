aeeebd1c277ad26b45dcee0eb6fd0a659cbf17d0
colemanl <evgshnajder@gmail.com>
2023-04-23 18:50:38 +0700

    1. Порядок геттеров переделал 2. отформатировал 3. Задал нужные данные (делал что бы не было варнингов) 4. listNames: переименовал в uniqueNamesString 5. notAdultsPeoples переименовал в notAdultPeople 6. средний возраст передалал через Collectors.averagingInt 7. averageAgeByNames переименовал в 7. averageAgesByNames 8. добавил проверку на наличие элеменот в коллекции

--- Lambdas/src/evgeny_shnayder/lambdas_main/Main.java
+++ Lambdas/src/evgeny_shnayder/lambdas_main/Main.java
@@ -13,12 +13,10 @@ public class Main {
                 new Person("Petr", 38),
                 new Person("Ivan", 28),
                 new Person("Petr", 32),
-                new Person("Roman", 11),
-                new Person("Maria", 17),
-                new Person("Lilia", 68));
-
-        personsList.get(0).setAge(24);
-        personsList.get(0).setName("Roman");
+                new Person("Roman", 17),
+                new Person("Maria", 11),
+                new Person("Lilia", 68)
+        );
 
         System.out.println("Список людей: " + personsList);
 
@@ -31,30 +29,33 @@ public class Main {
         System.out.println("Список людей с уникальными именами: " + uniqueNames);
 
         /* Б. вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр. */
-        String listNames = uniqueNames.stream()
+        String uniqueNamesString = uniqueNames.stream()
                 .collect(Collectors.joining(", ", "Имена: ", "."));
 
-        System.out.println("Список людей с уникальными именами. " + listNames);
+        System.out.println("Список людей с уникальными именами. " + uniqueNamesString);
 
         /* В. получить список людей младше 18, посчитать для них средний возраст */
 
-        List<Person> notAdultsPeoples = personsList.stream()
+        List<Person> notAdultPeople = personsList.stream()
                 .filter(person -> person.getAge() < 18)
                 .toList();
-        System.out.println("Список людей моложе 18 лет: " + notAdultsPeoples);
 
-        OptionalDouble averageAge = notAdultsPeoples.stream()
-                .mapToInt(Person::getAge)
-                .average();
+        if (notAdultPeople.isEmpty()) {
+            System.out.println("Людей моложе 18 лет нет.");
+        } else {
+            double average = notAdultPeople.stream()
+                    .collect(Collectors.averagingInt(Person::getAge));
 
-        System.out.println("Средний возраст людей моложе 18 лет: " + averageAge);
+            System.out.println("Список людей моложе 18 лет: " + notAdultPeople);
+            System.out.println("Средний возраст людей моложе 18 лет: " + average);
+        }
 
         /* Г. при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст */
 
-        Map<String, Double> averageAgeByNames = personsList.stream()
+        Map<String, Double> averageAgesByNames = personsList.stream()
                 .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
 
-        System.out.println("Средний возраст людей с группировкой по именам: " + averageAgeByNames);
+        System.out.println("Средний возраст людей с группировкой по именам: " + averageAgesByNames);
 
         /* Д. получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста */
 
